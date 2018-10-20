b2989189b707a5722fb4d35e725ea9ac98286627
Martin <martin.knavs@gmail.com>
2018-10-20 20:35:00 +0200

    Udemy: 5/85
    Using Time.deltaTime to make movement of the sheep framerate independent

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    
+    // configuration params
+    [SerializeField] float horizontalMoveSpeed = 10f;
+    [SerializeField] float verticalMoveSpeed = 10f;
+
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        Move();
+	}
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * horizontalMoveSpeed;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * verticalMoveSpeed;
+        var newXPos = transform.position.x + deltaX;
+        var newYPos = transform.position.y + deltaY;
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
